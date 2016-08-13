1826b8d02411194d644e2342c3a735f3e9cdd888
Matt Rice <mattrice12@outlook.com>
2016-08-13 15:19:09 -0500

    created tabs controller

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -1,23 +1,58 @@
 class TabsController < ApplicationController
   def index
+    tabs = Tab.all
+    render locals: { tabs: tabs }
   end
 
   def show
+    tab = Tab.find(params.fetch(:id))
+    if tab
+      render locals: { tab: tab }
+    else
+      redirect_to tabs
+    end
   end
 
   def new
+    render locals: {tab: Tab.new}
   end
 
   def create
+    tab = Tab.new(tab_params)
+    if tab.save
+      redirect_to tab
+    else
+      flash[:alert] = tab.errors
+      render template: 'tabs/new.html.erb', locals: { tab: tab}
+    end
   end
 
   def edit
+    render locals: { tab: Tab.find(params.fetch(:id)) }
   end
 
   def update
+    tab = Tab.find(params.fetch(:id))
+    if tab.update(tab_params)
+      redirect_to tab
+    else
+      render template: 'tabs/edit.html.erb', locals: { tab: tab }
+    end
   end
 
   def destroy
+    tab = Tab.find(id :params[id])
+    if tab.destroy
+      render message: "Take that, Greenpeace!"
+    else
+      render message: "Tab not found."
+    end
+  end
+
+  private
+
+  def tab_params
+    params.require(:tab).permit(:user_id, :parent_tab_id, :nickname)
   end
 
 end
--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -43,7 +43,7 @@ class TreesController < ApplicationController
   end
 
   def destroy
-    tree = Tree.find(id :params[id])
+    tree = Tree.find(id: params[:id])
     if tree.destroy
       render message: "Take that, Greenpeace!"
     else
