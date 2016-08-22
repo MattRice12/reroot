07c7e74adedde180a5e644e5d4a3c33d5c5edad1
Matt Rice <mattrice12@outlook.com>
2016-08-22 15:12:42 -0500

    got rid of a stray dot and made partials

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -1,4 +1,6 @@
 class Tab < ApplicationRecord
+  before_save :method_name
+
   has_many   :children, -> { order :id }, class_name: "Tab", foreign_key: :parent_tab_id
 
   belongs_to :parent, class_name: "Tab", foreign_key: :parent_tab_id, optional: true
@@ -11,6 +13,12 @@ class Tab < ApplicationRecord
       tab = tab.parent
     end
     tab.tree
+  end
+
+  def method_name
+    if self.name == ""
+      self.name = self.url
+    end
   end
 
 end
