855b1b26d3142808cbcbbf790d8aee764d98daa6
Matt Rice <mattrice12@outlook.com>
2016-08-25 16:53:24 -0500

    BROKEN DONT USE -- but has the SQL query for caching the tabs by descendents

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -8,6 +8,8 @@ class Tab < ApplicationRecord
   belongs_to :tree, optional: true
   belongs_to :user
 
+  scope :top_level, -> { where(parent_tab_id: nil) }
+
   def as_json(_ = nil)
     super(include: [:tab])
   end
@@ -19,6 +21,35 @@ class Tab < ApplicationRecord
     end
     tab.tree
   end
+
+  def descendents
+    self_and_descendents - [self]
+  end
+
+  def self_and_descendents
+    self.class.tree_for(self)
+  end
+
+  def self.tree_for(instance)
+    where("\"tabs\".id IN (#{tree_sql_for(instance)})") #.order("tabs.id")
+  end
+
+  def self.tree_sql_for(instance)
+   <<-SQL
+     WITH RECURSIVE search_tab(id, path) AS (
+         SELECT id, ARRAY[id]
+         FROM tabs
+         WHERE tabs.id = #{instance.id}
+       UNION ALL
+         SELECT tabs.id, path || tabs.id
+         FROM search_tab
+         JOIN tabs ON tabs.parent_tab_id = search_tab.id
+         WHERE NOT tabs.id = ANY(path)
+     )
+     SELECT id FROM search_tab ORDER BY path
+   SQL
+  end
+
 
   def default_tab_name
     if self.name == ""
