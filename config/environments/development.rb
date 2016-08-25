4dc9bac6789e82adff190e5cecd1ec91ba733cb3
Matt Rice <mattrice12@outlook.com>
2016-08-25 10:54:28 -0500

    added collection caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,15 +13,17 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
+  config.action_controller.perform_caching = true
+
   if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
+    # config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => 'public, max-age=172800'
     }
   else
-    config.action_controller.perform_caching = false
+    # config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
