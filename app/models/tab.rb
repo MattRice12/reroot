3ada49ca7780259cb0f0c7abd72b3015b7d1fcda
Matt Rice <mattrice12@outlook.com>
2016-08-26 12:15:20 -0500

    discovered that recursion doesn't substantially slow my page render down

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -22,38 +22,38 @@ class Tab < ApplicationRecord
     tab.tree
   end
 
-
-## These guys grab allllllllll tabs from the root tab down
-## This does not grab the tab from any particular position that you call the method
-  def descendants
-    self_and_descendants - [self]
-  end
-
-  def self_and_descandants
-    self.class.tree_for(self)
-  end
-
-  def self.tree_for(instance)
-    where("\"tabs\".id IN (#{tree_sql_for(instance)})") #.order("tabs.id")
-  end
-
-##This is a recursive search from the root tab to all the rest; it can only start where parent_tab_id = null
-  def self.tree_sql_for(instance)
-   <<-SQL
-     WITH RECURSIVE search_tab(id, path) AS (
-         SELECT id, ARRAY[id]
-         FROM tabs
-         WHERE parent_tab_id IS NULL AND
-         tabs.id = #{instance.id}
-       UNION ALL
-         SELECT tabs.id, path || tabs.id
-         FROM search_tab
-         JOIN tabs ON tabs.parent_tab_id = search_tab.id
-         WHERE NOT tabs.id = ANY(path)
-     )
-     SELECT id FROM search_tab ORDER BY path
-   SQL
-  end
+# 
+# ## These guys grab allllllllll tabs from the root tab down
+# ## This does not grab the tab from any particular position that you call the method
+#   def descendants
+#     self_and_descendants - [self]
+#   end
+#
+#   def self_and_descandants
+#     self.class.tree_for(self)
+#   end
+#
+#   def self.tree_for(instance)
+#     where("\"tabs\".id IN (#{tree_sql_for(instance)})") #.order("tabs.id")
+#   end
+#
+# ##This is a recursive search from the root tab to all the rest; it can only start where parent_tab_id = null
+#   def self.tree_sql_for(instance)
+#    <<-SQL
+#      WITH RECURSIVE search_tab(id, path) AS (
+#          SELECT id, ARRAY[id]
+#          FROM tabs
+#          WHERE parent_tab_id IS NULL AND
+#          tabs.id = #{instance.id}
+#        UNION ALL
+#          SELECT tabs.id, path || tabs.id
+#          FROM search_tab
+#          JOIN tabs ON tabs.parent_tab_id = search_tab.id
+#          WHERE NOT tabs.id = ANY(path)
+#      )
+#      SELECT id FROM search_tab ORDER BY path
+#    SQL
+#   end
 
   def default_tab_name
     if self.name == ""
