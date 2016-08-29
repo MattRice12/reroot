5934d012e6dc151e8d56e4d0f7b81e8ce19d8320
Matt Rice <mattrice12@outlook.com>
2016-08-29 13:30:17 -0500

    added test coverage checking gem (simplecov)

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,8 @@
 # newer version of cucumber-rails. Consider adding your own code to a new file
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
+require 'simplecov'
+SimpleCov.start
 
 require 'cucumber/rails'
 
@@ -55,4 +57,3 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
-
