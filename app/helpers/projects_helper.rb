61668acc9d256330af62fad5d7afdd181930be60
Matt Rice <mattrice12@outlook.com>
2016-08-30 13:16:15 -0500

    Contributions now calculate correctly

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,4 +6,14 @@ module ProjectsHelper
   def project_trees
     Project.find(params[:id]).trees
   end
+
+  def contributions(user)
+    # binding.pry
+
+    forests = Forest.where(project_id: params[:id])
+
+    trees = Tree.where(id: forests.map { |forest| forest.tree_id })
+    tab = Tab.where(tree_id: trees.each { |tree| tree.id }).where(user_id: user.id)
+    return tab
+  end
 end
